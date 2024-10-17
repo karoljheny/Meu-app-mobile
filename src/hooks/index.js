d774ecf73eb26ed389c57e5476599d5dd019ff66
Karol Jheny <136842282+karoljheny@users.noreply.github.com>
2024-10-17 18:43:10 +0000

    [feat] Expo-font

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
-import { Children } from "react"
 import { FontProvider } from "./font"
 
-export function AppProvider({}) {
-    return <FontProvider>{Children}</FontProvider>;
+export function AppProvider({children}) {
+    return <FontProvider>{children}</FontProvider>;
 }
